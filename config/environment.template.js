9a3d8ece22daa43b426bdbbfb1c02446bddeea75
Felix Rieseberg <felix@felixrieseberg.com>
2014-11-03 07:16:49 -1000

    Updated env config template

--- config/environment.template.js
+++ config/environment.template.js
@@ -1,66 +1,90 @@
 /* jshint node: true */
 
-module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'nitrous',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    // Torii authentification w/ google-auth
-    torii: {
-      providers: {
-        'google-oauth2': {
-          apiKey: '',
-          redirectUri: 'http://localhost:4200'
-        },
-        'facebook-oauth2': {
-          apiKey:      '',
-          redirectUri: 'http://localhost:4200'
+module.exports = function (environment) {
+    var ENV = {
+        modulePrefix: 'nitrous',
+        environment: environment,
+        baseURL: '/',
+        locationType: 'auto',
+    };
+
+    ENV['torii'] = {
+        providers: {
+            'google-oauth2': {
+                apiKey: '',
+                redirectUri: 'http://localhost:4200'
+            },
+            'facebook-oauth2': {
+                apiKey:      '',
+                redirectUri: 'http://localhost:4200'
+            }
         }
-      }
-    },
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
+    };
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
-  };
+    ENV['simple-auth'] = {
+        routeAfterAuthentication: 'dashboard',
+        routeIfAlreadyAuthenticated: 'dashboard'
+    };
 
-  ENV['simple-auth'] = {
-    routeAfterAuthentication: 'dashboard',
-    routeIfAlreadyAuthenticated: 'dashboard'
-  };
+    ENV['cordova'] = {
+        // Rebuild the cordova project on file changes. Blocks the server until it's
+        // finished.
+        //
+        // default: false
+        rebuildOnChange: true,
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.contentSecurityPolicyHeader = 'Disabled-Content-Security-Policy';
-  }
+        // Run the cordova emulate command after the build is finished
+        //
+        // default: false
+        emulate: true,
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+        // Which platform to build and/or emulate
+        //
+        // default: 'ios'
+        platform: 'ios',
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+        // Which URL the ember server is running on. This is used when using
+        // live-reload that comes with the starter kit.
+        //
+        // default: 'the-device-ip:4200'
+        emberUrl: 'http://10.0.1.12:4200',
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+        // Whether or not to use liveReload on the device simulator. Requires a few
+        // plugins to be installed that come with the starter-kit. It will cause your
+        // app to not boot up in the browser
+        //
+        // default: false and iOS
+        liveReload: {
+            enabled: false,
+            platform: 'ios'
+        }
+    };
 
-  if (environment === 'production') {
 
-  }
+    if (environment === 'development') {
+        // ENV.APP.LOG_RESOLVER = true;
+        ENV.APP.LOG_ACTIVE_GENERATION = true;
+        ENV.APP.LOG_TRANSITIONS = true;
+        ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        ENV.APP.LOG_VIEW_LOOKUPS = true;
+        ENV.contentSecurityPolicyHeader = 'Disabled-Content-Security-Policy';
+    }
 
-  return ENV;
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.baseURL = '/';
+        ENV.locationType = 'auto';
+
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+        ENV.APP.rootElement = '#ember-testing';
+    }
+
+    if (environment === 'production') {
+
+    }
+
+    return ENV;
 };
