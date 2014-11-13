89b8aa504c72dc15879b7c6f08a1b18ad4461cd7
Ivan R. Judson <irjudson@gmail.com>
2014-11-13 10:44:20 -0800

    Working on login flow.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,9 +3,7 @@ import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
     title: 'Sign In',
-    // authenticator: 'simple-auth-authenticator:oauth2-password-grant',
     authenticator: 'authenticator:nitrogen',
-
     actions: {
         authenticateWithGoogle: function () {
             var self = this;
--- app/views/device.js
+++ app/views/device.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-
     didInsertElement: function () {
         $("[type='checkbox']").bootstrapSwitch();
     }
