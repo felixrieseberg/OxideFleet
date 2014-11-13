0e3d015e109c448520cb272ffe41c073b85338b4
Felix Rieseberg <felix@felixrieseberg.com>
2014-11-13 10:49:08 -0800

    Intendation Fix

--- app/authenticators/nitrogen.js
+++ app/authenticators/nitrogen.js
@@ -16,66 +16,65 @@ export default Base.extend({
         nitrogenService = new nitrogen.Service(Config.APP.nitrogen);
     },
 
-/**
-Restores the session from a set of session properties.
-@method restore
-@param {Object} data The data to restore the session from
-@return {Ember.RSVP.Promise} A promise that when it resolves results in the session being authenticated
-*/
-restore: function(data) {
-    var _this = this;
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-        console.log("Nitrogen authenticator restore, with data: " + data);
-        var principal = new nitrogen.User({
-            accessToken: {
-                token: data.accessToken.token
-            },
-            id: data.user.id,
-            nickname: data.user.nickname
-        });
-        nitrogenService.resume(principal, function (err, user, accessToken) {
-            if (err) { reject(err); }
-            resolve({ user: user.principal, accessToken: user.accessToken });
-        });
-    });
-},
-
-/**
-Authenticates the session with the specified `credentials`.
-@method authenticate
-@param {Object} credentials The credentials to authenticate the session with
-@return {Ember.RSVP.Promise} A promise that resolves when an access token is successfully acquired from the server and rejects otherwise
-*/
-authenticate: function(credentials) {
-    var _this = this;
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-        console.log("Nitrogen authenticator authenticate.");
-        var user = new nitrogen.User({
-            nickname: 'current',
-            email: credentials.identification,
-            password: credentials.password
-        });
-        Ember.run(function () {
-            nitrogenService.authenticate(user, function (err, user, accessToken) {
+    /**
+    Restores the session from a set of session properties.
+    @method restore
+    @param {Object} data The data to restore the session from
+    @return {Ember.RSVP.Promise} A promise that when it resolves results in the session being authenticated
+    */
+    restore: function(data) {
+        var _this = this;
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+            console.log("Nitrogen authenticator restore, with data: " + data);
+            var principal = new nitrogen.User({
+                accessToken: {
+                    token: data.accessToken.token
+                },
+                id: data.user.id,
+                nickname: data.user.nickname
+            });
+            nitrogenService.resume(principal, function (err, user, accessToken) {
                 if (err) { reject(err); }
                 resolve({ user: user.principal, accessToken: user.accessToken });
             });
         });
-    });
-},
+    },
 
-/**
-Cancels any outstanding automatic token refreshes and returns a resolving promise.
-@method invalidate
-@param {Object} data The data of the session to be invalidated
-@return {Ember.RSVP.Promise} A resolving promise
-*/
-invalidate: function(data) {
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-        console.log("Nitrogen authenticator invalidate.");
-        nitrogenService = null;
-        resolve({ user: null, accessToken: null });
-    });
-},
+    /**
+    Authenticates the session with the specified `credentials`.
+    @method authenticate
+    @param {Object} credentials The credentials to authenticate the session with
+    @return {Ember.RSVP.Promise} A promise that resolves when an access token is successfully acquired from the server and rejects otherwise
+    */
+    authenticate: function(credentials) {
+        var _this = this;
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+            console.log("Nitrogen authenticator authenticate.");
+            var user = new nitrogen.User({
+                nickname: 'current',
+                email: credentials.identification,
+                password: credentials.password
+            });
+            Ember.run(function () {
+                nitrogenService.authenticate(user, function (err, user, accessToken) {
+                    if (err) { reject(err); }
+                    resolve({ user: user.principal, accessToken: user.accessToken });
+                });
+            });
+        });
+    },
 
+    /**
+    Cancels any outstanding automatic token refreshes and returns a resolving promise.
+    @method invalidate
+    @param {Object} data The data of the session to be invalidated
+    @return {Ember.RSVP.Promise} A resolving promise
+    */
+    invalidate: function(data) {
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+            console.log("Nitrogen authenticator invalidate.");
+            nitrogenService = null;
+            resolve({ user: null, accessToken: null });
+        });
+    }
 });
