532327818bad043d60f6f8a704658be9d4dff6f0
Felix Rieseberg <felix@felixrieseberg.com>
2014-11-25 19:41:19 -0800

    Introduce Draconian Grunt Codestyle Checks
    - Include both JSHint and JSCS
    - Added draconian rules

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    var files = ['gruntfile.js', 'app/**/*.js'];
+
+    grunt.initConfig({
+        jshint: {
+            files: files,
+            options: {
+                jshintrc: './.jshintrc'
+            }
+        },
+        jscs: {
+            files: {
+                src: files
+            },
+            options: {
+                config: '.jscsrc',
+                esnext: true
+            }
+        }
+    });
+
+    grunt.registerTask('codestyle', ['jshint', 'jscs']);
+    grunt.registerTask('default', []);
+};
