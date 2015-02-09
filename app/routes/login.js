8cb07451018c8431473814a85c0d7d5330f0fc39
Felix Rieseberg <felix@felixrieseberg.com>
2015-02-09 09:48:48 -0800

    Provide Error on Incorrect Login

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
-    beforeModel : function () {
+    beforeModel: function () {
         this.controllerFor('application').set('hideSidebar', true);
     },
 
