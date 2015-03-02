4f483e0ad83a1b4fbc3e0d0142c580f50d7316e0
David Douglas <david@deadlyfingers.com>
2015-03-02 15:34:45 +0000

    Slide sidebar in/out button added. (This closes #6)

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,6 +7,39 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
         sessionInvalidationSucceeded: function () {
             // Force reload to empty all cached data
             window.location = 'http://' + document.location.host;
+        },
+
+        toggleSidebar: function () {
+            var width = $('#sidebar-container').width();
+            var slideOut = ($('#sidebar-container').css('left') !== '-'+width+'px') ? true : false;
+            var rightIndent = '0px';
+
+            if (slideOut === true) {
+                $('#sidebar-container').animate({
+                    left: -width
+                }, { duration: 200, queue: false });
+                $('#main-container').animate({
+                    marginLeft: '0px',
+                    marginRight: '0px'
+                }, { duration: 200, queue: false });
+                $('#navbar').animate({
+                    marginLeft: '0px',
+                    marginRight: '0px'
+                }, { duration: 200, queue: false });
+            } else {
+                $('#sidebar-container').animate({
+                    left: '0px'
+                }, { duration: 200, queue: false });
+                $('#main-container').animate({
+                    marginLeft: width,
+                    marginRight: rightIndent
+                }, { duration: 200, queue: false });
+                $('#navbar').animate({
+                    marginLeft: width,
+                    marginRight: rightIndent
+                }, { duration: 200, queue: false });
+            }
         }
     }
+
 });
