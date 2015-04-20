7e4678c19ffae825772a2d2b3fb97d1636e21489
Felix Rieseberg <felix@felixrieseberg.com>
2015-04-19 22:06:07 -0700

    Working Hamburger

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,5 +4,21 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     model: function () {
         return this.store.find('device');
+    },
+
+    actions: {
+        toggleSideNav: function () {
+            var $menu = Ember.$('.hamburger'),
+                $container = Ember.$('.main-container'),
+                $containerMargin = $container.css('margin-left');
+
+            // If the menu isn't out, pull it out
+            if ($containerMargin !== '250px') {
+                $container.animate({"margin-left": "250px"}, { duration: 200, queue: false}); 
+            } else {
+                $container.animate({"margin-left": "50px"}, { duration: 200, queue: false}); 
+            }
+            
+        }
     }
 });
