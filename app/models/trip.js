8da6a6422232bd9f00ced140a7cdc41833e4739d
Felix Rieseberg <felix@felixrieseberg.com>
2015-05-12 16:02:03 -0700

    Update Models

--- app/models/trip.js
+++ app/models/trip.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
 
+/**
+ * This model describes a trip as it exists on a vehicle.
+ * @type {DS.model}
+ */
 export default DS.Model.extend({
-  vehicle: DS.belongsTo('vehicle', {async: true}),
-  tripEvents: DS.hasMany('event', {embedded: 'always', async: true}),
-  driver: DS.belongsTo('driver', {async: true})
+    vehicle: DS.belongsTo('vehicle', {async: true}),
+    tripEvents: DS.hasMany('event', {embedded: 'always', async: true}),
+    driver: DS.belongsTo('driver', {async: true})
 });
--- app/models/vehicle.js
+++ app/models/vehicle.js
@@ -1,13 +1,16 @@
 import DS from 'ember-data';
 
-// This model should probably be attached to device
+/**
+ * This model describes the vehicle property on the 'device'
+ * @type {DS.Model}
+ */
 export default DS.Model.extend({
-  trips: DS.hasMany('trip', {async: true}),
-  name: DS.attr('string'),
-  is_active: DS.attr('boolean'),
-  vin: DS.attr('string'),
-  make: DS.attr('string'),
-  model: DS.attr('string'),
-  production_year: DS.attr('string'),
-  mileage: DS.attr('string')
+    trips: DS.hasMany('trip', {async: true}),
+    name: DS.attr('string'),
+    is_active: DS.attr('boolean'),
+    vin: DS.attr('string'),
+    make: DS.attr('string'),
+    model: DS.attr('string'),
+    production_year: DS.attr('string'),
+    mileage: DS.attr('string')
 });
