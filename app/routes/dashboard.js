1dad50d705d7e348b2747d2d065d95a24cbffadf
Tyler Gibson <tgibson@microsoft.com>
2015-05-13 02:08:44 -0700

    Adding collapse and close buttons to cards

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -14,6 +14,21 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
             // If the menu isn't out, pull it out
             $container.toggleClass('expanded');
             $menu.toggleClass('expanded');
+        },
+
+        toggleVehicleList: function () {
+            var $container = Ember.$('.vehicle-list'),
+                $icon = Ember.$('.vehicle-list > .card-content > .card-title > i');
+
+            $container.toggleClass('collapsed');
+            if ($icon.hasClass('mdi-navigation-expand-less')) {
+                $icon.removeClass('mdi-navigation-expand-less');
+                $icon.addClass('mdi-navigation-expand-more');
+            } else {
+                $icon.removeClass('mdi-navigation-expand-more');
+                $icon.addClass('mdi-navigation-expand-less');
+            }
+
         }
     }
 });
